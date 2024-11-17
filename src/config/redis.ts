f32963a9d841f86331e7a8dd0afe5a49350e229c
iknow6aint <saintagbukor@gmail.com>
2024-11-17 16:23:30 +0100

    ggs

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,11 +1,11 @@
-import RedisClient from '../utils/RedisClient';
+// import RedisClient from '../utils/RedisClient';
 
-export const connectRedis = async () => {
-  try {
-    await RedisClient.connect();
-    console.log('Connected to Redis');
-  } catch (err) {
-    console.error('Error connecting to Redis:', err);
-    process.exit(1);
-  }
-};
+// export const connectRedis = async () => {
+//   try {
+//     await RedisClient.connect();
+//     console.log('Connected to Redis');
+//   } catch (err) {
+//     console.error('Error connecting to Redis:', err);
+//     process.exit(1);
+//   }
+// };
